fe1f799a4af5cdc6b8e9544aac8bd59a7aa2d2ea
nbur4556 <nbur4556@gmail.com>
2020-11-06 13:14:52 -0600

    organized public directory references

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.static("public"));
+
 app.get('/', (req, res) => {
-    res.send("hello world");
+    res.sendFile(index.html);
 });
 
 app.listen(PORT, () => {
