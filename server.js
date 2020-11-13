e645eee8f2bdb7e9dee789acb67770cc089a13ec
nbur4556 <nbur4556@gmail.com>
2020-11-12 19:40:26 -0600

    removed compression, did not make a difference

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require('express');
-const compression = require('compression')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(compression());
 app.use(express.static("public"));
 
 // Home
